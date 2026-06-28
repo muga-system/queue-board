6ebdd4a2ddc33692435323bf8ceb5762acf176af
Juan di Corato <mugadevelop@gmail.com>
2026-06-28 16:48:11 -0300

    feat: renderiza trabajos simulados

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,6 +30,43 @@ const pendingJobs = jobs
   .filter((job) => job.status === queueStates.pending)
   .sort((firstJob, secondJob) => firstJob.priority - secondJob.priority);
 
-console.log("Estados disponibles", queueStates);
-console.log(jobs);
-console.log(pendingJobs);
+function getSortedJobsByStatus(status) {
+  const jobsByStatus = jobs.filter((job) => job.status === status);
+
+  if (status !== queueStates.pending) {
+    return jobsByStatus;
+  }
+  return jobsByStatus.sort(
+    (firstJob, secondJob) => firstJob.priority - secondJob.priority,
+  );
+}
+
+function createJobElement(job) {
+  const jobElement = document.createElement("article");
+  jobElement.classList.add("job-card");
+
+  jobElement.innerHTML = `<h3>${job.title}</h3>
+  <p>Prioriddad ${job.priority}</p>`;
+  return jobElement;
+}
+
+function renderJobs() {
+  const colums = document.querySelectorAll("[data-status]");
+
+  colums.forEach((column) => {
+    const status = column.dataset.status;
+    const jobsList = column.querySelector("[data-jobs-list]");
+    const jobsByStatus = getSortedJobsByStatus(status);
+
+    jobsList.innerHTML = "";
+
+    jobsByStatus.forEach((job) => {
+      const jobElement = createJobElement(job);
+      jobsList.appendChild(jobElement);
+    });
+  });
+}
+
+renderJobs();
+
+console.log("Trabajos renderizados:", jobs);
